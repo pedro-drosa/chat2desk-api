2307dfe9fc4692d4819b51ae7e2b718234f3ccae
pedro-drosa <pedro_drosa@hotmail.com>
2021-12-14 08:35:53 -0300

    feature: Controllers setup

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,9 @@
-import { Request, Response } from "express";
 import { Router } from 'express';
+import UserController from '../controllers/UserController';
 
 const usersRouter = Router();
+const userController = new UserController();
 
-usersRouter.get('/users', (req: Request, res: Response)=> res.json({message: 'Hello Chat2Desk'}));
+usersRouter.get('/users', userController.store);
 
 export default usersRouter;
