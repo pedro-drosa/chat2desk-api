8be15eaa16bd2559cbc1ea9e8a86a04d35b2c97b
pedro-drosa <pedro_drosa@hotmail.com>
2021-12-14 19:49:21 -0300

    feature: jwt validation

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import authMiddleware from '../middlewares/auth';
 
 const usersRouter = Router();
 const userController = new UserController();
 
 usersRouter.post('/users', userController.store);
-usersRouter.get('/users', userController.index);
+usersRouter.get('/users', authMiddleware, userController.index);
 
 export default usersRouter;
--- src/services/CreateTokenService.ts
+++ src/services/CreateTokenService.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export default class CreateTokenService {
   public execute(key: string) {
-    return jwt.sign({id: key}, process.env.JWT_SECRET as string, {
+    return jwt.sign({id: key}, '7a5707a5235990e0db55b8e197162d10', {
       expiresIn: 86400
     });
   }
